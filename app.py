914e31c95ec065b7ed2a508285906cfd5dfdb6e9
Eva Ou <speeder528@gmail.com>
2024-12-24 21:22:29 +0800

    transform input data format

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ input_data = pd.DataFrame({
 geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
 geo_encoded_df = pd.DataFrame(geo_encoded, columns = onehot_encoder_geo.get_feature_names_out(['Geography']))
 
+input_data = pd.DataFrame([input_data])
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis = 1)
 
 input_data_scaled = scaler.transform(input_data)
